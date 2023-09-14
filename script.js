e38134b32922bce293f35f9889aa9d221f97cb4a
Phil Peraza <phil.peraza@outlook.com>
2023-09-14 00:11:31 -0700

    add/ create counter function

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function double(value) {
-    if (Array.isArray(value)){
-        return value.map(function (num) {
-            return num*2
-        });
-    } 
-    return value * 2;
-}
-console.log(double(2)); // 4
-console.log(double([1, 2, 3])); // [2, 4, 6]
+function counter() {
+    // write your code here
+  }
+  
+  counter(); // 1
+  counter(); // 2
+  counter(); // 3
+  counter(); // 4
+  counter(); // 5
+
+  console.log(counter);
